df03a2fcc01af56b428f6b4f2a26ee538bb312a3
thekayaurora <onenonlyclyron@gmail.com>
2025-10-31 02:41:59 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: "https://kay.is-a.dev",
+  site: "http://kay.is-a.dev",
 });
