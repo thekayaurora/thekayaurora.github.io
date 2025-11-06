b367df8ee046ae22b1e5a35c0b889278c89b238a
thekayaurora <onenonlyclyron@gmail.com>
2025-11-06 23:06:37 +0200

    fixes tee em

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
+import remarkBreaks from 'remark-breaks'; /* i shouldnt need to use a PLUGIN just to properly do a hard break ffs */
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
   site: "http://kay.is-a.dev",
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+  },
 });
